bea6254ddfb56d36c2c71590a27b6cb58ee1fe67
章伟东 <zhangweidong@zwd-Pro.local>
2017-05-31 16:45:20 +0800

    threee types of deep clone

---
+++ deepclone.js
@@ -0,0 +1,53 @@
+function isArray(obj) {
+    return Object.prototype.toString.call(obj) == "[object Array]"
+}
+
+function deepclone(obj) {
+    if (typeof obj != "object" || obj == null)
+        return obj;
+    var target = isArray(obj) ? [] : {};
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            typeof obj[key] == "obj" ? clone(obj[key]) : target[key] = obj[key]
+
+        }
+    }
+    return target;
+}
+
+
+
+function deep2(obj) {
+    if (obj == null || typeof (obj) != "object")
+        return obj;
+    var temp = new obj.constructor();
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            temp[key] = deep2(obj[key])
+        }
+    }
+    return temp;
+
+}
+
+function deep3(obj) {
+    return JSON.parse(JSON.stringify(obj))
+}
+
+var a1 = function () {
+    console.log("hello a1")
+}
+var b1 = deep(a1);
+console.log(b1);
+
+var a2 = ["1", "2"]
+var b2 = clone2(a2);
+console.log(b2)
+
+
+var a3 = { name: "a3", address: { isLocal: true } }
+var b3 = deep3(a3);
+console.log(b3)
+
+var b4 = deep3(a4);
+console.log(b4);
