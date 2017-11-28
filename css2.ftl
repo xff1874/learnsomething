7c3a1b658969da0156766b0b3569b7d01f5014d0
hzzhangweidong <hzzhangweidong@corp.netease.com>
2017-11-28 16:17:18 +0800

    将sdk inject 到ftl

---
+++ css2.ftl
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>My App</title>
+  </head>
+  <body>
+  <div>
+  sdfsfds
+  
+  </div>
+  <p>
+  <!-- start:js -->
+  <script src="ef65926e9eec945df97a.bundle.js"></script>
+  <!-- end:js -->
+    </p>
+  
+  </body>
+</html>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
+var InjectHtmlPlugin = require('inject-html-webpack-plugin')
+
+
 module.exports = {
     entry: './deepclone.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: '.'
+    },
     output: {
-      filename: 'bundle.js'
-    }
+        library: "jStore",
+        libraryTarget: "umd",
+        filename: '[chunkhash].bundle.js',
+        auxiliaryComment: {
+            root: "Root Comment",
+            commonjs: "CommonJS comment",
+            commonjs2: "CommonJS2 Comment",
+            amd: "AMD Comment"
+        }
+    },
+    plugins: [new InjectHtmlPlugin({
+        filename: 'css2.ftl',
+        chunks: ['main'],
+        customInject: [{
+            start: '<!-- start:js -->',
+            end: '<!-- end:js-->'
+        }]
+    })]
 };
